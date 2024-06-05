400f730ad6a2b5a7d83b9bdb9c7839a7b2138f85
wendellavila <wendelljcavila@gmail.com>
2024-06-05 08:19:04 -0300

    ci: Added GitHub Actions deployment file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? process.env.NEXT_PUBLIC_BASE_PATH : undefined;
+
+const nextConfig = {
+    basePath: basePath,
+    trailingSlash: true,
+    output: 'export',
+    images: {
+        unoptimized: true
+    },
+    serverRuntimeConfig: {
+        PROJECT_ROOT: __dirname
+    }
+}
 
 export default nextConfig;
